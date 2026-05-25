e490a6f84561011965ab1338156b922e5e87c627
Tarun <tarunpotnuru12@gmail.com>
2026-05-25 11:28:32 +0530

    5

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 import numpy as np
 import os
-home_dir = os.getenv("HOME")
+home_dir = os.getenv("HOME_SECRET")
 print(f"Home directory: {home_dir}")
 print("Hello world")
 print("Every thing is  working fine")
